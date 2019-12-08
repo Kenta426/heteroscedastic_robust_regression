96a46b1a2d8492f50d2f3102f152693d90be4dbb
Kenta Takatsu <kt426@cornell.edu>
2019-12-08 09:48:09 -0500

    update model

--- model/torch_model.py
+++ model/torch_model.py
@@ -3,10 +3,14 @@ import torch.nn as nn
 
 
 class PolyRegression(nn.Module):
-    def __init__(self, dim):
+    def __init__(self, dim, init_zeros=False):
         super(PolyRegression, self).__init__()
         self.beta = nn.Linear(dim, 1)
         self.n_poly = dim
+        if init_zeros:
+            with torch.no_grad():
+                self.beta.weight.copy_(torch.zeros(dim))
+                self.beta.bias.copy_(torch.zeros(1))
 
     def forward(self, x):
         """
@@ -15,11 +19,3 @@ class PolyRegression(nn.Module):
         expand = [(x**(i+1)).view(-1, 1) for i in range(self.n_poly)]
         expand = torch.cat(expand, dim=1)
         return self.beta(expand)
-
-
-class AdaptiveRegression(nn.Module):
-    def __init__(self):
-        pass
-
-    def forward(self, x):
-        pass
